352ab99522c7ce5d3a9bd33400c782a4502c315a
Ram <rameshwarpatil9689@gmail.com>
2025-08-06 10:53:33 +0530

    new repo for Blog-app

---
+++ megablog/src/Components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo({width='100px'}) {
+  return (
+    <div>
+      <img 
+      className='w-20 h-25'
+      src="/images/blog-removebg-preview.png" alt="" />
+    </div>
+  )
+}
+
+export default Logo
